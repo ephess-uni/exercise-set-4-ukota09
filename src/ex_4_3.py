d2bfb572ad119eac37c74ff3a8479ceb1b59b4e3
ukota09 <123261012+ukota09@users.noreply.github.com>
2023-02-25 23:39:28 -0600

    Update ex_4_3.py

--- src/ex_4_3.py
+++ src/ex_4_3.py
@@ -5,13 +5,10 @@ try:
     from src.ex_4_0 import get_shutdown_events
     from src.ex_4_2 import logstamp_to_datetime
     from src.util import get_data_file_path
-    from datetime import datetime
 except ImportError:
     from ex_4_0 import get_shutdown_events
     from ex_4_2 import logstamp_to_datetime
     from util import get_data_file_path
-    from datetime import datetime
-
 
 # Use this FILENAME variable to test your function.
 FILENAME = get_data_file_path("messages.log")
@@ -19,15 +16,17 @@ FILENAME = get_data_file_path("messages.log")
 
 
 def time_between_shutdowns(logfile):
-    
     """
     Your docstring here.  Replace the pass keyword below with your implementation.
     """
-    sh_list = get_shutdown_events(logfile)
-    formatstr = "INFO %Y-%m-%dT%H:%M:%S supybot Shutdown initiated."
-    first = datetime.strptime(sh_list[0], formatstr)
-    last = datetime.strptime(sh_list[-1], formatstr)
-    return last - first
+    items = get_shutdown_events(logfile)
+    new_items = [item.split()[1] for item in items]
+    obj = [logstamp_to_datetime(item) for item in new_items]
+    time_between = obj[-1] - obj[0]
+    return time_between
+
+
+
 # >>>> The code below will call your function and print the results
-if _name_ == "_main_":
+if __name__ == "__main__":
     print(f'{time_between_shutdowns(FILENAME)=}')
